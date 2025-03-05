ac91c97c7c32d9d8c5f7b0b0a034d364670867bc
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2025-03-05 14:28:12 -0500

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -49,7 +49,8 @@ def save_to_faiss(chunks: list[Document]):
         shutil.rmtree(FAISS_PATH)
 
     # Initilize embedding model
-    embeddings = OllamaEmbeddings(model="mxbai-embed-large") 
+    #embeddings = OllamaEmbeddings(model="mxbai-embed-large") 
+    embeddings = OllamaEmbeddings(model="tinyllama") 
 
     # Create a new DB from the documents.
     db = FAISS.from_documents(chunks, embeddings)
